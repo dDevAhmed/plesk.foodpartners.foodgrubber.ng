0d6aaea4a0c34564b06ade053688473c8b228d48
ahmed <ahmedmahmud1994@hotmail.com>
2024-03-13 12:28:41 +0100

    must verify email page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -31,10 +31,6 @@
 
     <meta name="description" content="" />
 
-    <!-- Favicon -->
-    {{-- fixme - use original favicon --}}
-    {{-- <link rel="icon" type="image/x-icon" href="vendors/sneat/assets/img/favicon/favicon.ico" /> --}}
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
